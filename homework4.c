0ad1bbf600e08041826c3e2ad02100dc10922c3b
hitesh500 <61031511+hitesh500@users.noreply.github.com>
2020-04-08 00:18:05 -0400

    Add files via upload

--- homework4.c
+++ homework4.c
@@ -7,32 +7,51 @@ int main(void)
     char *response = "\n\n\r2534 is the best course in the curriculum!\r\n\n";
 
     // TODO: Declare the variables that main uses to interact with your state machine.
-
+    bool finished = false;
 
     // Stops the Watchdog timer.
     initBoard();
     // TODO: Declare a UART config struct as defined in uart.h.
+   eUSCI_UART_ConfigV1 uartConfig = {
+        EUSCI_A_UART_CLOCKSOURCE_SMCLK,
+       19,
+        8,
+        0x55,
+        EUSCI_A_UART_NO_PARITY,
+        EUSCI_A_UART_LSB_FIRST,
+        EUSCI_A_UART_ONE_STOP_BIT,
+        EUSCI_A_UART_MODE,
+        EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION,
+        EUSCI_A_UART_8_BIT_LEN
+    };
     //       To begin, configure the UART for 9600 baud, 8-bit payload (LSB first), no parity, 1 stop bit.
-
-
     // TODO: Make sure Tx AND Rx pins of EUSCI_A0 work for UART and not as regular GPIO pins.
-
+       GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1, GPIO_PIN2, GPIO_PRIMARY_MODULE_FUNCTION);
+       GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P1, GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
 
     // TODO: Initialize EUSCI_A0
-
+       UART_initModule(EUSCI_A0_BASE, &uartConfig);
 
     // TODO: Enable EUSCI_A0
+       UART_enableModule(EUSCI_A0_BASE);
 
 
     while(1)
     {
         // TODO: Check the receive interrupt flag to see if a received character is available.
         //       Return 0xFF if no character is available.
-
+        if (UART_getInterruptStatus(EUSCI_A0_BASE,EUSCI_A_UART_TRANSMIT_FLAG)==0){
+            rChar=0xFF;
+        }
+        else {
+            rChar=0x00;
+        }
 
         // TODO: If an actual character was received, echo the character to the terminal AND use it to update the FSM.
         //       Check the transmit interrupt flag prior to transmitting the character.
+        if (rChar!=0xFF){
 
+        }
 
 
         // TODO: If the FSM indicates a successful string entry, transmit the response string.
@@ -43,6 +62,7 @@ int main(void)
     }
 }
 
+
 void initBoard()
 {
     WDT_A_hold(WDT_A_BASE);
@@ -52,7 +72,7 @@ void initBoard()
 bool charFSM(char rChar)
 {
     bool finished = false;
-
+    switch
 
     return finished;
 }
